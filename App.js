d84a2b1c700b5b5a101db73d60e5845fc3411dc8
Alejandra Barros <alejandra.barros@aluno.senai.br>
2025-03-25 15:57:20 -0300

    feat: create page with text inputs and buttons

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Text, TextInput, Button, TouchableOpacity, StyleSheet } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function HomeScreen() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.titulo}>Preencha os campos</Text>
+            <TextInput placeholder="Nome" style={styles.input} />
+            <TextInput placeholder="Turma" style={styles.input} />
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>CONFIRMAR</Text>
+            </TouchableOpacity>
+            <Button title="Refazer" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        backgroundColor: "#E0FFFF",
+        height: "100vh",
+        width: "100vw",
+        padding: 20,
+        gap: 20,
+    },
+    titulo: {
+        fontSize: 20,
+        fontWeight: "800",
+        color: "#4682B4",
+        textAlign: "center",
+    },
+    input: {
+      width: 400,
+      height: 40,
+      padding: 10,
+      borderWidth: 1.5,
+      borderColor: "#4682B4",
+      backgroundColor: "#F0FFFF",
+      borderRadius: 5,
+      color: "#4682B4",
+      fontWeight: "700",
+    },
+    button: {
+      width: 400,
+      height: 40,
+      backgroundColor: "#4682B4",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      borderRadius: 5,
+    },
+    buttonText: {
+      color: "#F0FFFF",
+      fontWeight: "700",
+    }
+
 });
